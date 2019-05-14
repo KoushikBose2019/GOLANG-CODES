02c099d1008de5474482fb1bc30635099ec6daaf
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-14 23:46:33 +0530

    decision making in GO LANG

---
+++ decisionmaking.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+func main()
+{
+var a int=100
+if(a<=24)
+{
+	fmt.Printf(" a is less than 20\n")
+}
+else if(a==20)
+{
+	fmt.Printf("a is not less than 20\n")
+}
+else if(a==40)
+{
+	fmt.Printf("arnok")
+}
+else if(a==100)
+{
+	fmt.Printf("tony")
+}
+else
+{
+	fmt.Printf("raman")
+}
+
+}
