bf18dcc10fdb0a8d50c65d8dbcf88cc6adebf784
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-14 21:12:42 +0530

    constant variable

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+	const LENGTH int=14
+	const WIDTH int=24
+	var area int=LENGTH*WIDTH
+	fmt.Println("value of the area: ",area)
+	fmt.Printf("value of the area:%d",area)
+	fmt.Println("hello\ngo!")
+	fmt.Println("hello\tgo!")
+	fmt.Println("hello\bgo!")
+	fmt.Println("hello\ago!")
+
+
+}
