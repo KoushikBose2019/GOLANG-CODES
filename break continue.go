b03e8e6ab004fd8adcff31862d7cfdef8f12ad11
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-15 12:48:39 +0530

    Add files via upload

---
+++ break continue.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+func main(){
+var a int=10
+for a<20{
+	fmt.Printf("value of a is %d\n",a)
+	a++
+	if a>15{
+		break
+	}
+	for a<20{
+		if a==15{
+			a++
+			continue
+		}
+		fmt.Printf("value of a is %d\n",a)
+		a++
+	}
+}
+
+
+
+
+
+
+
+
+
+}
