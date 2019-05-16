8faa0bf9a6591fbce3c14440ef1d03034ae98697
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-16 15:13:00 +0530

    String

---
+++ string.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+func main(){
+/*var greeting="hello world!"
+
+fmt.Printf("normal string: ",greeting)
+fmt.Printf("%d",greeting)
+fmt.Printf("\n")
+fmt.Printf("hex byte : ",)
+
+for  i:=0;i<len(greeting);i++{
+	fmt.Printf("%d",greeting[i])
+}
+fmt.Printf("\n")
+const sampletext="\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+
+fmt.Println("quoted string: ")
+fmt.Println("%+q",sampletext)
+fmt.Println("\n")
+fmt.Printf("type of the variable is:%T\n",sampletext)*/
+var greeting="hello world!"
+fmt.Println(" strength length is : ",len(greeting))
+//fmt.Println(len(greeting))
+
+}
