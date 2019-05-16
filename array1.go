fda7e7185034ad760c997e0a6eb5a9e2efeee4dc
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-05-16 21:49:38 +0530

    array proagramme

---
+++ array1.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main(){
+var arr=[]int{1,2,3,4}
+var i int
+var x[14] int
+var i,j int
+for i =0;i<14;i++{
+	x[i]=i+10
+}
+	for j=0;j<14;j++{
+		fmt.Printf("Element[%d]=%d\n",i,x[j])
+	}
+}
+for i=0;i<len(arr);i++{
+fmt.Println(arr)
+}
+}
